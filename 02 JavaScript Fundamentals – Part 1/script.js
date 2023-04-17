6da98f91aba811651acdb88dc6c02bfd3241e805
nathis-s <nathis.ct20@bitsathy.ac.in>
2023-04-17 15:52:52 +0530

    17/04/23-1

---
+++ 02 JavaScript Fundamentals – Part 1/script.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+
+alert("Hello world!");
+
+
+let variable1="Welcome to js";
+if(variable1=="Welcom to js"){
+    alert("True");
+}
+else{
+    alert("No");
+}
+
+
+console.log(34+32+45+76);
+
+
